d214f49b81eb5bfe9283fa3d961245e348e365fc
Maximiliano Arias <marias@demand.io>
2023-12-31 18:52:33 -0300

    chore: update favicon and powered by

--- src/modules/common/components/head/index.tsx
+++ src/modules/common/components/head/index.tsx
@@ -14,7 +14,26 @@ const Head: React.FC<HeadProps> = ({ title, description, image }) => {
       <meta itemProp="name" content={title} />
       {description && <meta itemProp="description" content={description} />}
       {image && <meta itemProp="image" content={image} />}
+
       <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
     </NextHead>
   )
 }
